c3a902539c85dfe334e0482e9e18bc26e4229d29
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-13 12:52:32 +0100

    Integrate needed modules

--- tictac.js
+++ tictac.js
@@ -1,200 +1,145 @@
+// Player Factory Function
+const Player = (name, symbol) => {
+    return {
+        getName: () => name,
+        getSymbol: () => symbol,
+    };
+};
+
+// Gameboard Module
 const Gameboard = (() => {
-    // Private properties
-    let board = [];
-  
-    // Public methods
+    let board = Array(9).fill(null);
+
     const initializeBoard = () => {
-      board = Array(9).fill(null); // Initialize the board with 9 empty cells
+        board = Array(9).fill(null);
     };
-  
+
     const makeMove = (player, index) => {
-      if (board[index] === null) {
-        board[index] = player.symbol;
-        return true; // Move successful
-      } else {
-        return false; // Cell is already occupied
-      }
-    };
-  
-    const checkWin = (player) => {
-      const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ];
-  
-      for (const combination of winningCombinations) {
-        const [a, b, c] = combination;
-        if (board[a] === player.symbol && board[b] === player.symbol && board[c] === player.symbol) {
-          return true; // Player has won
+        if (board[index] === null) {
+            board[index] = player.getSymbol();
+            return true;
         }
-      }
-  
-      return false; // No winner yet
+        return false;
     };
-  
+
+    const checkWin = (player) => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6],
+        ];
+
+        return winningCombinations.some(combination => {
+            return combination.every(index => {
+                return board[index] === player.getSymbol();
+            });
+        });
+    };
+
     const isFull = () => {
-      return board.every((cell) => cell !== null);
+        return board.every(cell => cell !== null);
     };
-  
-    const resetBoard = () => {
-      initializeBoard();
-    };
-  
-    return {
-      initializeBoard,
-      makeMove,
-      checkWin,
-      isFull,
-      resetBoard,
-    };
-  })();
-  
-  // Example usage:
-  // Gameboard.initializeBoard();
-  // Gameboard.makeMove(player1, 0);
-  // Gameboard.checkWin(player1);
-  // Gameboard.isFull();
-  // Gameboard.resetBoard();
-  
-  const Player = (name, symbol) => {
-    // Properties
-    const playerName = name;
-    const playerSymbol = symbol;
-  
-    // Public methods
-    const getName = () => playerName;
-    const getSymbol = () => playerSymbol;
-  
-    return {
-      getName,
-      getSymbol,
-    };
-  };
-  
-  // Example usage:
-  // const player1 = Player("Player 1", "X");
-  // const player2 = Player("Player 2", "O");
-  // const playerName = player1.getName();
-  // const playerSymbol = player2.getSymbol();
 
-  const GameController = (() => {
-    // Private properties
+    const getBoard = () => {
+        return [...board];
+    };
+
+    return {
+        initializeBoard,
+        makeMove,
+        checkWin,
+        isFull,
+        getBoard,
+    };
+})();
+
+// GameController Module
+const GameController = (() => {
     let players = [];
-    let currentPlayer = null;
-    let gameboard = null;
-  
-    // Public methods
-    const initializeGame = (player1, player2, board) => {
-      players = [player1, player2];
-      gameboard = board;
-      currentPlayer = players[0];
-      displayController.updateStatus(`${currentPlayer.getName()}'s turn`);
+    let currentPlayerIndex = 0;
+
+    const initializeGame = (player1, player2) => {
+        players = [player1, player2];
+        currentPlayerIndex = 0;
+        Gameboard.initializeBoard();
+        DisplayController.updateStatus(`${getCurrentPlayer().getName()}'s turn`);
+        DisplayController.renderBoard();
     };
-  
+
+    const getCurrentPlayer = () => players[currentPlayerIndex];
+
     const switchPlayer = () => {
-      currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     };
-  
+
     const handleMove = (index) => {
-      if (!gameboard.makeMove(currentPlayer, index)) {
-        return; // Cell already occupied, do nothing
-      }
-  
-      displayController.renderBoard();
-      
-      if (gameboard.checkWin(currentPlayer)) {
-        displayController.showResult(`${currentPlayer.getName()} wins!`);
-        return;
-      }
-  
-      if (gameboard.isFull()) {
-        displayController.showResult("It's a tie!");
-        return;
-      }
-  
-      switchPlayer();
-      displayController.updateStatus(`${currentPlayer.getName()}'s turn`);
+        if (Gameboard.makeMove(getCurrentPlayer(), index)) {
+            DisplayController.renderBoard();
+
+            if (Gameboard.checkWin(getCurrentPlayer())) {
+                DisplayController.showResult(`${getCurrentPlayer().getName()} wins!`);
+                return;
+            }
+
+            if (Gameboard.isFull()) {
+                DisplayController.showResult("It's a tie!");
+                return;
+            }
+
+            switchPlayer();
+            DisplayController.updateStatus(`${getCurrentPlayer().getName()}'s turn`);
+        }
     };
-  
-    const endGame = () => {
-      players = [];
-      currentPlayer = null;
-      gameboard.resetBoard();
-    };
-  
+
     return {
-      initializeGame,
-      handleMove,
-      endGame,
+        initializeGame,
+        handleMove,
     };
-  })();
-  
-  // Example usage:
-  // GameController.initializeGame(player1, player2, Gameboard);
-  // GameController.handleMove(0);
-  // GameController.endGame();
+})();
 
-
-
-  const DisplayController = (() => {
-    // Private properties
+// DisplayController Module
+const DisplayController = (() => {
     const boardContainer = document.querySelector("#board-container");
     const statusDisplay = document.querySelector("#status-display");
     const restartButton = document.querySelector("#restart-button");
-  
-    // Public methods
+
     const renderBoard = () => {
-      const board = gameboard.getBoard();
-      boardContainer.innerHTML = "";
-  
-      board.forEach((cell, index) => {
-        const cellElement = document.createElement("div");
-        cellElement.classList.add("cell");
-        cellElement.textContent = cell || ""; // Display X or O in the cell
-        cellElement.addEventListener("click", () => cellClickHandler(index));
-        boardContainer.appendChild(cellElement);
-      });
+        const board = Gameboard.getBoard();
+        boardContainer.innerHTML = "";
+
+        board.forEach((cell, index) => {
+            const cellElement = document.createElement("button");
+            cellElement.classList.add("cell");
+            cellElement.textContent = cell || "";
+            cellElement.setAttribute("data-index", index);
+            cellElement.addEventListener("click", () => GameController.handleMove(index));
+            boardContainer.appendChild(cellElement);
+        });
     };
-  
+
     const updateStatus = (message) => {
-      statusDisplay.textContent = message;
+        statusDisplay.textContent = message;
     };
-  
+
     const showResult = (result) => {
-      statusDisplay.textContent = result;
-      restartButton.style.display = "block";
+        updateStatus(result);
+        restartButton.style.display = "block";
     };
-  
-    const initListeners = () => {
-      restartButton.addEventListener("click", () => {
+
+    restartButton.addEventListener("click", () => {
         restartButton.style.display = "none";
-        GameController.endGame();
-        renderBoard();
-      });
-    };
-  
-    const cellClickHandler = (index) => {
-      if (!gameboard.isGameOver()) {
-        GameController.handleMove(index);
-      }
-    };
-  
+        GameController.initializeGame(player1, player2);
+    });
+
     return {
-      renderBoard,
-      updateStatus,
-      showResult,
-      initListeners,
+        renderBoard,
+        updateStatus,
+        showResult,
     };
-  })();
-  
-  // Example usage:
-  // DisplayController.renderBoard();
-  // DisplayController.updateStatus("Player X's turn");
-  // DisplayController.showResult("Player O wins!");
-  // DisplayController.initListeners();
+})();
+
+// Initialize players and game
+const player1 = Player("Player 1", "X");
+const player2 = Player("Player 2", "O");
+
+GameController.initializeGame(player1, player2);
